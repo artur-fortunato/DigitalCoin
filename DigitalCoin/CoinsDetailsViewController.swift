9b72de7147fdacd439611481cbc172c695019870
Artur <Artur>
2021-01-22 01:06:49 -0300

    construindo a tela de detalhes programaticamente, sem storyboard nem xib

---
+++ DigitalCoin/CoinsDetailsViewController.swift
@@ -0,0 +1,217 @@
+//
+//  MainViewController.swift
+//  DigitalCoin
+//
+//  Created by Artur Rodrigues Fortunato on 21/01/21.
+//
+
+import UIKit
+import SnapKit
+
+class CoinsDetailsViewController: UIViewController {
+
+    let greenColor = UIColor(red: 139/255, green: 153/255, blue: 90/255, alpha: 1)
+    let fontColor = UIColor(red: 230/255, green: 233/255, blue: 212/255, alpha: 1)
+    let blackColor = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+    
+    
+    
+    private lazy var coinDetailView: UIView = {
+        let view = UIView()
+        
+        view.backgroundColor = greenColor
+        return view
+    }()
+    
+    private lazy var buttonBack: UIButton = {
+        let button = UIButton()
+        button.setTitle("Voltar", for: .normal)
+        button.setTitleColor(fontColor, for: .normal)
+        //button.addAction(self, for: .touchDown)
+        
+        return button
+    }()
+    
+    private lazy var lblID: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 25, weight: .medium)
+        label.textAlignment = .center
+        label.textColor = fontColor
+        label.text = "ID"
+        return label
+    }()
+    
+    private lazy var icon: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    
+    private lazy var lblValue: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 40, weight: .medium)
+        label.textAlignment = .center
+        label.textColor = fontColor
+        label.text = "$ 31,010.20"
+        return label
+    }()
+    
+    private lazy var buttonAdd: UIButton = {
+        let button = UIButton()
+        button.setTitle("ADICIONAR", for: .normal)
+        button.setTitleColor(fontColor, for: .normal)
+        //button.addAction(self, for: .touchDown)
+        
+        return button
+    }()
+    
+    private lazy var tradeView: UIView = {
+        let view = UIView()
+        view.backgroundColor = blackColor
+        
+        return view
+    }()
+    
+    private lazy var tradedVolume: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 25, weight: .medium)
+        label.textAlignment = .center
+        label.textColor = fontColor
+        label.text = "volumes negociados"
+        return label
+    }()
+    
+    
+    init(){
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        setupViewConfiguration()
+    }
+
+
+}
+
+//
+extension CoinsDetailsViewController: ViewConfiguration{
+   
+
+    func buildViewHierarchy() {
+
+        view.addSubview(coinDetailView)
+        coinDetailView.addSubview(buttonBack)
+        coinDetailView.addSubview(lblID)
+        coinDetailView.addSubview(icon)
+        coinDetailView.addSubview(lblValue)
+        coinDetailView.addSubview(buttonAdd)
+        view.addSubview(tradeView)
+        tradeView.addSubview(tradedVolume)
+    }
+    
+    func setupConstraints() {
+        
+        coinDetailView.snp.makeConstraints { (make) in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(0)
+            make.left.equalTo(view).offset(0)
+            make.right.equalTo(view).inset(0)
+            make.height.equalTo(300)
+        }
+        
+        buttonBack.snp.makeConstraints { (make) in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.left.equalTo(view).offset(8)
+        }
+        
+        lblID.snp.makeConstraints { (make) in
+            make.top.equalTo(coinDetailView).offset(20)
+            make.left.equalTo(coinDetailView).offset(10)
+            make.right.equalTo(coinDetailView).inset(10)
+        }
+            
+        icon.snp.makeConstraints { (make) in
+            make.top.equalTo(lblID.snp.bottom).offset(20)
+            make.left.equalTo(coinDetailView).offset(100)
+            make.right.equalTo(coinDetailView).inset(100)
+
+            make.height.equalTo(30)
+        }
+        
+        lblValue.snp.makeConstraints { (make) in
+            make.top.equalTo(icon.snp.bottom).offset(20)
+            make.left.equalTo(coinDetailView).offset(10)
+            make.right.equalTo(coinDetailView).inset(10)
+
+        }
+       
+        buttonAdd.snp.makeConstraints { (make) in
+            make.top.equalTo(lblValue.snp.bottom).offset(30)
+            make.left.equalTo(coinDetailView).offset(10)
+            make.right.equalTo(coinDetailView).inset(10)
+
+            make.height.equalTo(40)
+        }
+        
+        tradeView.snp.makeConstraints { (make) in
+            make.top.equalTo(coinDetailView.snp.bottom).offset(0)
+            make.left.equalTo(view).offset(0)
+            make.right.equalTo(view).inset(0)
+            make.height.equalTo(500)
+        }
+        
+        tradedVolume.snp.makeConstraints { (make) in
+            make.top.equalTo(tradeView).offset(20)
+            make.left.equalTo(tradeView).offset(30)
+            make.right.equalTo(tradeView).inset(30)
+        }
+    }
+
+    func configureViews() {
+        view.backgroundColor = .white
+    }
+}
+//
+//
+
+
+
+
+
+
+
+
+
+//        ShowCoinsView()
+//        ShowTradeView()
+
+    
+
+    
+
+    
+//    func ShowCoinsView(){
+//        _ = UINib.init(nibName: "CoinsDetailsView", bundle: nil)
+//        let rootView = Bundle.main.loadNibNamed("CoinsDetailsView", owner: self, options: nil)?.first as? CoinsDetailsView
+//        if let coinsDetailsView = rootView{
+//            coinsDetailsView.tag = 100
+//            //coinsDetailsView.frame = CGRect(x:0, y: 0, width: 375, height: 300)
+//            self.navigationController?.view.addSubview(coinsDetailsView)
+//        }
+//    }
+//
+//    func ShowTradeView(){
+//        _ = UINib.init(nibName: "TradeDetailsView", bundle: nil)
+//        let rootView = Bundle.main.loadNibNamed("TradeDetailsView", owner: self, options: nil)?.first as? TradeDetailsView
+//        if let tradeDetailsView = rootView{
+//            tradeDetailsView.tag = 100
+//            //tradeDetailsView.frame = CGRect(x:0, y: 300, width: 375, height: 396)
+//            self.navigationController?.view.addSubview(tradeDetailsView)
+//        }
+//    }
