4c8217382c06c11e9dbb52bf914567ba6a2e401f
aloisiofjrdev <aloisiofjr@hotmail.com>
2021-01-22 15:08:13 -0300

    Melhoria na API

--- DigitalCoin/ApiViewController/ViewController.swift
+++ DigitalCoin/ApiViewController/ViewController.swift
@@ -16,6 +16,11 @@ class ViewController: UIViewController {
     var getAllName = [String]()
     var getAllUsd = [Double]()
     var getAllUrl = [String]()
+    var getAllAssetId = [String]()
+    var getAllVolumeHrs = [Double]()
+    var getAllVolumeDay = [Double]()
+    var getAllVolumeMth = [Double]()
+    
     
 
     override func viewDidLoad() {
@@ -23,7 +28,10 @@ class ViewController: UIViewController {
         getDataName()
         getDataUsd()
         getDataImagem()
-        
+        getDataAssetId()
+        getDataVolumeHrs()
+        getDataVolumeDay()
+        getDataVolumeMth()
     }
     
     func getDataName() {
@@ -56,6 +64,51 @@ class ViewController: UIViewController {
         }
     }
     
+    func getDataVolumeHrs() {
+        CoinsAPI().getCoins { (coinsArray, erro) in
+            if let error = erro {
+                print(error)
+            }else if let coins = coinsArray{
+                self.coins = coins
+                for x in 0..<coins.count{
+                    let allSingleValue = coins[x]
+                    let allVolumeHrs = allSingleValue.volume1HrsUsd
+                    self.getAllVolumeHrs.append(allVolumeHrs ?? 0.00)
+                }
+            }
+        }
+    }
+    
+    func getDataVolumeDay() {
+        CoinsAPI().getCoins { (coinsArray, erro) in
+            if let error = erro {
+                print(error)
+            }else if let coins = coinsArray{
+                self.coins = coins
+                for x in 0..<coins.count{
+                    let allSingleValue = coins[x]
+                    let allVolumeDay = allSingleValue.volume1DayUsd
+                    self.getAllVolumeDay.append(allVolumeDay ?? 0.00)
+                }
+            }
+        }
+    }
+    
+    func getDataVolumeMth() {
+        CoinsAPI().getCoins { (coinsArray, erro) in
+            if let error = erro {
+                print(error)
+            }else if let coins = coinsArray{
+                self.coins = coins
+                for x in 0..<coins.count{
+                    let allSingleValue = coins[x]
+                    let allVolumeMth = allSingleValue.volume1MthUsd
+                    self.getAllVolumeMth.append(allVolumeMth ?? 0.00)
+                }
+            }
+        }
+    }
+    
     func getDataImagem() {
         CoinsAPIImagem().getCoins2 { (coinsArray, erro) in
             if let error = erro {
@@ -67,10 +120,27 @@ class ViewController: UIViewController {
                     let allUrl = allSingleValue.url
                     self.getAllUrl.append(allUrl)
                 }
-                print (self.getAllUrl)
+                //print (self.getAllUrl)
             }
         }
     }
+    
+    func getDataAssetId() {
+        CoinsAPI().getCoins { (coinsArray, erro) in
+            if let error = erro {
+                print(error)
+            }else if let coins = coinsArray{
+                self.coins = coins
+                for x in 0..<coins.count{
+                    let allSingleValue = coins[x]
+                    let allAssetId = allSingleValue.assetID
+                    self.getAllAssetId.append(allAssetId)
+                }
+            }
+        }
+    }
+    
+    
 }
     
     
