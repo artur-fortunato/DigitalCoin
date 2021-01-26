672b6717c4ee1fa6014dcf86b4a1f74947955fc4
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2021-01-26 15:26:26 -0300

    inclusao CoinsListView utilizando MVVM

--- DigitalCoin/View/TelaPrincipal/TelaPrincipalTableViewCell.swift
+++ DigitalCoin/View/TelaPrincipal/TelaPrincipalTableViewCell.swift
@@ -9,7 +9,19 @@ import UIKit
 
 class TelaPrincipalTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var imageSimboloCoin: UIImageView!
     @IBOutlet weak var labelnomeMoeda: UILabel!
+    
+    @IBOutlet weak var labelSiglaCoin: UILabel!
+    @IBOutlet weak var labelValorCoin: UILabel!
+    
+    var modelCoins: CoinsModel?{
+        didSet{
+            userConfiguration()
+        }
+    }
+    
+    // MARK - Inicializadores
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,4 +34,26 @@ class TelaPrincipalTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    // MARK - Func
+    
+    func userConfiguration(){
+        guard let name = modelCoins?.name else{return}
+        guard let sigla = modelCoins?.id else{return}
+        guard let usd = modelCoins?.priceUsd else{return}
+        let usdFormatado = formatNumberToDecimal(value: usd)
+        
+        labelnomeMoeda.text = name
+        labelSiglaCoin.text = sigla
+        labelValorCoin.text = usdFormatado
+        
+    }
+    
+    func formatNumberToDecimal (value:Double) -> String{
+        let numformatado = NumberFormatter()
+        
+        numformatado.minimumFractionDigits = 2
+        numformatado.numberStyle = .decimal
+        
+        return numformatado.string(from: NSNumber(value:value)) ?? "00"
+    }
 }
